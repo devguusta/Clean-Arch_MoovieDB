089ff08ff16c0f2a7e66ed5c5341a2ba29842a78
Gustavo Silva Candido Cabral <gustavo.cabral@rarolabs.com.br>
2022-02-10 10:45:03 -0300

    feat: create cew entity

---
+++ lib/moovies/domain/entities/crew.dart
@@ -0,0 +1,10 @@
+class Crew {
+  final String realName;
+  final String characterName;
+  final String? profile;
+  Crew({
+    required this.realName,
+    required this.characterName,
+    this.profile,
+  });
+}
